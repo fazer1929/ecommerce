4565f815e4a65d694d0ab65aa550d7d6a008ba7d
Abhishek Agrawal <abhishekagrawal8888@gmail.com>
2020-09-12 03:34:25 +0530

    Updates

---
+++ precache-manifest.8c053cfd761d609f5ee14a196a3256b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09ef981ce609a2b471690c522c4c3c0",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "b736ac38636300a0ded2",
+    "url": "/ecommerce/static/css/main.36048eef.chunk.css"
+  },
+  {
+    "revision": "f1b15c26eaacd350b80c",
+    "url": "/ecommerce/static/js/2.0e17ec50.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/ecommerce/static/js/2.0e17ec50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b736ac38636300a0ded2",
+    "url": "/ecommerce/static/js/main.bd08e48f.chunk.js"
+  },
+  {
+    "revision": "ef2c8be2bac792de925a",
+    "url": "/ecommerce/static/js/runtime-main.68e96947.js"
+  }
+]);
